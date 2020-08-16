3d37fc80cfb591e5fedf73762bc77ee5b0b3b088
Alberto <albertosolpal@gmail.com>
2020-08-16 17:47:39 +0200

    Added fetching btc-eur

--- server/index.js
+++ server/index.js
@@ -7,7 +7,34 @@ const { prisma } = require('./generated/javascript-client')
 
 const resolvers = {
   Query: {
-    coins: (parent, args, ctx, info) => ctx.prisma.coins({}, info),
+    coins: (parent, args, context, info) => context.prisma.coins({}, info),
+  },
+  Mutation: {
+    updateCoin: (parent, args, context, info) => {
+      const updatedCoin = context.prisma.updateCoin({
+        where: { id: args.id },
+        data: {
+          base: args.base,
+          target: args.target,
+          price: args.price
+        },
+      }, info)
+      return newCoin
+    },
+  },
+  Subscription: {
+    coins: {
+      subscribe: (parent, args, context, info) => {
+        return context.prisma.coin(
+          {
+            where: {
+              mutation_in: ['CREATED', 'UPDATED'],
+            },
+          },
+          info,
+        )
+      },
+    },
   },
 };
 
@@ -30,17 +57,28 @@ const options = {
 server.start(options, args => {
 
   setInterval(async () => {
-
     try {
 
+    // Fetch data from the api
+    const response = await axios.get('https://api.cryptonator.com/api/ticker/btc-eur')
+
+    const { ticker: { base, target, price } } = response.data
+
+    // Update db with new fetched data
+    await prisma.updateCoin({
+      where: { id: '5f393fde02743900072e4140' },
+      data: {
+        base: base,
+        target: target,
+        price: price
+      },
+    })
     const coins = await prisma.coins()
     console.log("Coins", coins)
 
-      // const response = await axios.get('https://api.cryptonator.com/api/ticker/btc-eur')
     } catch (err) {
       console.log(err)
     }
-
-  }, 3000)
+  }, 30000)
   console.log(`Server is running on http://localhost:${args.port}`)
 })
